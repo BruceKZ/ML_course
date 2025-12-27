245a569ebbaef0954eb7fb4ac270bce8ec884066
Zhixiang Dai <hnhnuh@126.com>
2025-12-27 15:40:37 +0100

    Lab03

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -12,4 +12,13 @@ def build_poly(x, degree):
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
     # ***************************************************
-    raise NotImplementedError
+    return np.vander(x, degree + 1, increasing=True)
+
+
+def build_poly_2(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    ### SOLUTION
+    poly = np.ones((len(x), 1))
+    for deg in range(1, degree + 1):
+        poly = np.c_[poly, np.power(x, deg)]
+    return poly
